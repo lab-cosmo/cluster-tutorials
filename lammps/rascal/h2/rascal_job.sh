0eb70f4b54e431b5643fa6805c1575aa028b4b11
alexgo <alex.goscinski@posteo.de>
2021-08-23 17:22:38 +0200

    add rascal examples

---
+++ lammps/rascal/h2/rascal_job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=ipi
+#SBATCH --get-user-env
+#SBATCH --partition=jobs
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=00:10:00
+
+module purge
+module load gcc
+module load lammps
+
+mpirun -n 1 lmp_mpi -i h2.in.lammps > log.lammps
+exit 0
+
