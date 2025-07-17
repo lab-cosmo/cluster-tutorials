954fd8dc5892cb5144c6482c71ef8d2a0e75a950
Cesare Malosso <72449246+cesaremalosso@users.noreply.github.com>
2025-07-17 11:00:36 +0200

    Update initial_cache.cmake

--- FHI-aims/MAD-2/jed_settings/initial_cache.cmake
+++ FHI-aims/MAD-2/jed_settings/initial_cache.cmake
@@ -1,14 +1,13 @@
 ## This was taken from FHIaims/cmake/toolchains/intel.cmake version 250320. Any modifcations made are indicated.
 
-set(TARGET_NAME aims.250610.scalapack.hdf5.mpi.cosmo.mad.2 CACHE STRING "")
 set(CMAKE_INSTALL_PREFIX "./" CACHE STRING "")
 
 ###############
 # Basic Flags #
 ###############
 set(CMAKE_Fortran_COMPILER mpiifort CACHE STRING "")
-set(CMAKE_Fortran_FLAGS "-O3 -ip -fp-model precise" CACHE STRING "")
-set(Fortran_MIN_FLAGS "-O0 -fp-model precise" CACHE STRING "")
+set(CMAKE_Fortran_FLAGS "-O2 -fallow-argument-mismatch -ffree-line-length-none" CACHE STRING "")
+set(Fortran_MIN_FLAGS "-O0 -ffree-line-length-none" CACHE STRING "")
 set(INC_PATHS "$ENV{HDF5_ROOT}/include" CACHE STRING "")  # added
 set(LIB_PATHS "$ENV{MKLROOT}/lib/intel64 $ENV{HDF5_HOME}/lib" CACHE STRING "")  # modified to include HDF5 lib
 set(HDF5_Fortran_INCLUDE_DIR "$ENV{HDF5_ROOT}/include" CACHE STRING "")  # added
@@ -18,10 +17,10 @@ set(USE_COOP ON CACHE STRING "")
 ###############
 # C,C++ Flags #
 ###############
-set(CMAKE_C_COMPILER icc CACHE STRING "")
-set(CMAKE_C_FLAGS "-O3 -ip -fp-model precise -std=gnu99" CACHE STRING "")
-set(CMAKE_CXX_COMPILER icpc CACHE STRING "")
-set(CMAKE_CXX_FLAGS "-O3 -ip -fp-model precise" CACHE STRING "")
+set(CMAKE_C_COMPILER  icx CACHE STRING "")
+set(CMAKE_C_FLAGS "-O2" CACHE STRING "")
+set(CMAKE_CXX_COMPILER icpx CACHE STRING "")
+set(CMAKE_CXX_FLAGS "-O2" CACHE STRING "")
 
 set(ELPA2_KERNEL "" CACHE STRING "Change to AVX/AVX2/AVX512 if running on Intel processors")
 
