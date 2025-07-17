888a03747e5af570917b6ae5bac64d7b11f2bec1
Cesare Malosso <72449246+cesaremalosso@users.noreply.github.com>
2025-07-17 11:01:01 +0200

    Update run-cmake.sh

--- FHI-aims/MAD-2/jed_settings/run-cmake.sh
+++ FHI-aims/MAD-2/jed_settings/run-cmake.sh
@@ -1,6 +1,10 @@
 #/bin/bash
 
 module purge
-module load cmake intel intel-oneapi-mkl intel-oneapi-mpi elpa hdf5/1.12.2-mpi
+module load cmake
+module load intel-oneapi-compilers/2024.1.0
+module load intel-oneapi-mpi/2021.12.1
+module load intel-oneapi-mkl/2024.0.0
+module load hdf5/1.14.3-mpi
 
 cmake -C initial_cache.cmake ../
